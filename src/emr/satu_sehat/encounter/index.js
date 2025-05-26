b3496f6bd81948a052fc187284f9de9fa8878143
rsudbendan <it-bendan@gmail.com>
2025-05-26 13:18:17 +0700

    perbarui dan cari encounter

--- src/emr/satu_sehat/encounter/index.js
+++ src/emr/satu_sehat/encounter/index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const koneksi = require("../../../koneksi");
 const { convertToISO } = require("../../../fungsi");
 const { fungsi_generate_token } = require("../fungsi");
-const { buat } = require("../../../satu_sehat/encounter/index");
+const { buat, cari_by_id_pasien, perbarui } = require("../../../satu_sehat/encounter/index");
 
 const router = express.Router();
 
@@ -54,68 +54,6 @@ router.post("/buat", async (req, res) => {
     }
 
     let token = hasil_generate_token.data.token
-
-    // fungsi utama
-    // let body = {
-    //     resourceType: "Encounter",
-    //     status: "arrived",
-    //     class: {
-    //         system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
-    //         code: "AMB",
-    //         display: "ambulatory"
-    //     },
-    //     subject: {
-    //         reference: "Patient/P03647103112",
-    //         display: "patient 2"
-    //     },
-    //     participant: [
-    //         {
-    //             type: [
-    //                 {
-    //                     coding: [
-    //                         {
-    //                             system: "http://terminology.hl7.org/CodeSystem/v3-ParticipationType",
-    //                             code: "ATND",
-    //                             display: "attender"
-    //                         }
-    //                     ]
-    //                 }
-    //             ],
-    //             individual: {
-    //                 reference: "Practitioner/10006926841",
-    //                 display: "dr. Yoga Yandika, Sp.A"
-    //             }
-    //         }
-    //     ],
-    //     period: {
-    //         start: "2022-06-14T07:00:00+07:00"
-    //     },
-    //     location: [
-    //         {
-    //             location: {
-    //                 reference: "Location/b017aa54-f1df-4ec2-9d84-8823815d7228",
-    //                 display: "Ruang 1A, Poliklinik Bedah Rawat Jalan Terpadu, Lantai 2, Gedung G"
-    //             }
-    //         }
-    //     ],
-    //     statusHistory: [
-    //         {
-    //             status: "arrived",
-    //             period: {
-    //                 start: "2022-06-14T07:00:00+07:00"
-    //             }
-    //         }
-    //     ],
-    //     serviceProvider: {
-    //         reference: "Organization/7688ce50-bdb3-43c5-9777-b58c9effe8f8"
-    //     },
-    //     identifier: [
-    //         {
-    //             system: "http://sys-ids.kemkes.go.id/encounter/7688ce50-bdb3-43c5-9777-b58c9effe8f8",
-    //             value: "P20240001"
-    //         }
-    //     ]
-    // };
 
     let body = {
         resourceType: "Encounter",
@@ -182,7 +120,7 @@ router.post("/buat", async (req, res) => {
         let hasil_buat = await buat(token, body);
 
         let id_encounter = "";
-        if (hasil_buat.status === 200) {
+        if (hasil_buat.status.toString().substring(0, 1) === "2") {
             id_encounter = hasil_buat.data.id;
         }
 
@@ -225,6 +163,205 @@ router.post("/buat", async (req, res) => {
             status: hasil_buat.status,
             message: hasil_buat.message,
             data: hasil_buat.data
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Terjadi kesalahan : " + error.message
+        });
+    }
+})
+
+router.post("/cari_by_id_pasien", async (req, res) => {
+    if (!req.body || !req.body.id_pasien) {
+        return res.status(400).json({
+            status: 400,
+            message: "Parameter belum lengkap"
+        });
+    }
+
+    let id_pasien = req.body.id_pasien;
+
+    // generate token
+    let hasil_generate_token = await fungsi_generate_token();
+    if (hasil_generate_token.status !== 200) {
+        return res.status(hasil_generate_token.status).json({
+            status: hasil_generate_token.status,
+            message: hasil_generate_token.message
+        })
+    }
+
+    let token = hasil_generate_token.data.token
+
+    try {
+        let hasil_cari_by_id_pasien = await cari_by_id_pasien(token, id_pasien);
+
+        return res.status(hasil_cari_by_id_pasien.status).json({
+            status: hasil_cari_by_id_pasien.status,
+            message: hasil_cari_by_id_pasien.message,
+            data: hasil_cari_by_id_pasien.data
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Terjadi kesalahan : " + error.message
+        });
+    }
+})
+
+router.post("/perbarui", async (req, res) => {
+    // if (!req.body || !req.body.nik) {
+    //     return res.status(400).json({
+    //         status: 400,
+    //         message: "Parameter belum lengkap"
+    //     });
+    // }
+
+    if (!req.body) {
+        return res.status(400).json({
+            status: 400,
+            message: "Parameter belum lengkap"
+        });
+    }
+
+    let id_encounter = req.body.id_encounter;
+    let noreg = req.body.noreg;
+    let status = req.body.status;
+    let class_code = req.body.class_code;
+    let class_display = req.body.class_display;
+    let subject_patient_id = req.body.subject_patient_id;
+    let subject_patient_name = req.body.subject_patient_name;
+    let participant_coding_code = req.body.participant_coding_code;
+    let participant_coding_display = req.body.participant_coding_display;
+    let participant_individual_id = req.body.participant_individual_id;
+    let participant_individual_display = req.body.participant_individual_display;
+    let period_start = convertToISO(req.body.period_start);
+    let location_id = req.body.location_id;
+    let location_display = req.body.location_display;
+    let history_status = req.body.history_status;
+    let history_start = convertToISO(req.body.history_start);
+
+    // generate token
+    let hasil_generate_token = await fungsi_generate_token();
+    if (hasil_generate_token.status !== 200) {
+        return res.status(hasil_generate_token.status).json({
+            status: hasil_generate_token.status,
+            message: hasil_generate_token.message
+        })
+    }
+
+    let token = hasil_generate_token.data.token
+
+    let body = {
+        resourceType: "Encounter",
+        id: id_encounter,
+        identifier: [
+            {
+                system: "http://sys-ids.kemkes.go.id/encounter/" + process.env.SATU_SEHAT_ORGANIZATION_ID,
+                value: noreg
+            }
+        ],
+        status: status,
+        class: {
+            system: "http://terminology.hl7.org/CodeSystem/v3-ActCode",
+            code: class_code,
+            display: class_display
+        },
+        subject: {
+            reference: "Patient/" + subject_patient_id,
+            display: subject_patient_name
+        },
+        participant: [
+            {
+                type: [
+                    {
+                        coding: [
+                            {
+                                system: "http://terminology.hl7.org/CodeSystem/v3-ParticipationType",
+                                code: participant_coding_code,
+                                display: participant_coding_display
+                            }
+                        ]
+                    }
+                ],
+                individual: {
+                    reference: "Practitioner/" + participant_individual_id,
+                    display: participant_individual_display
+                }
+            }
+        ],
+        period: {
+            start: period_start,
+            end: period_start
+        },
+        location: [
+            {
+                location: {
+                    reference: "Location/" + location_id,
+                    display: location_display
+                }
+            }
+        ],
+        statusHistory: [
+            {
+                status: history_status,
+                period: {
+                    start: history_start
+                }
+            }
+        ],
+        serviceProvider: {
+            reference: "Organization/" + process.env.SATU_SEHAT_ORGANIZATION_ID
+        }
+    };
+
+    try {
+        let hasil_perbarui = await perbarui(token, body, id_encounter);
+
+        // let id_encounter = "";
+        // if (hasil_perbarui.status.toString().substring(0, 1) === "2") {
+        //     id_encounter = hasil_perbarui.data.id;
+        // }
+
+        let json_response = JSON.stringify(hasil_perbarui.data, null, 2);
+
+        // let connection;
+        // try {
+        //     connection = await koneksi.koneksi_rsbendan.getConnection();
+
+        //     // Memulai transaksi
+        //     await connection.beginTransaction();
+
+        //     // INSERT
+        //     await connection.query(`
+        //         INSERT INTO 
+        //             satusehat_encounter_baru ( 
+        //                 id_encounter,
+        //                 environment, 
+        //                 norm, 
+        //                 noreg, 
+        //                 json_response, 
+        //                 waktu_entry 
+        //             )
+        //         VALUES
+        //         (?, ?, ?, ?, ?, now())
+        //     `, [id_encounter, process.env.SATU_SEHAT_ENVIRONMENT, norm, noreg, json_response]);
+
+        //     await connection.commit();
+        // } catch (error) {
+        //     if (connection) await connection.rollback();
+        //     return ({
+        //         status: 400,
+        //         message: "Terjadi kesalahan dalam koneksi database : " + error.message
+        //     });
+        // } finally {
+        //     if (connection) connection.release();
+        // }
+
+        return res.status(hasil_perbarui.status).json({
+            status: hasil_perbarui.status,
+            message: hasil_perbarui.message,
+            data: hasil_perbarui.data
         });
     } catch (error) {
         return res.status(500).json({
--- src/satu_sehat/encounter/index.js
+++ src/satu_sehat/encounter/index.js
@@ -43,6 +43,86 @@ const buat = async (token, body) => {
     }
 }
 
+const cari_by_id_pasien = async (token, id_pasien) => {
+    try {
+        let url = `${process.env.SATU_SEHAT_BASE_URL}/Encounter?subject=${id_pasien}`;        
+
+        let response = await axios.get(url, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+
+        let pesan = "Sukses";
+        let status = response.status;
+        if (response.data.total === 0){
+            status = 206;
+            pesan = "Data tidak ditemukan";
+        }
+
+        return {
+            status: status,
+            message: pesan,
+            data: response.data
+        };
+    } catch (error) {
+        let pesan = "";
+        if (error.response.status.toString().substring(0,1) === "4"){
+            pesan = "Error dari klien : ";
+        }
+        else if (error.response.status.toString().substring(0,1) === "5"){
+            pesan = "Error dari Satu Sehat : ";
+        }
+
+        return {
+            status: error.response.status,
+            message: pesan + error.response.data.issue[0].details.text,
+            data: error.response.data
+        }
+    }
+}
+
+const perbarui = async (token, body, id_encounter) => {
+    try {
+        let url = `${process.env.SATU_SEHAT_BASE_URL}/Encounter/${id_encounter}`;        
+
+        let response = await axios.put(url, body, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+
+        let pesan = "Sukses";
+        let status = response.status;
+
+        return {
+            status: status,
+            message: pesan,
+            data: response.data
+        };
+    } catch (error) {
+        let pesan = "";
+        if (error.response.status.toString().substring(0,1) === "4"){
+            pesan = "Error dari klien : ";
+        }
+        else if (error.response.status.toString().substring(0,1) === "5"){
+            pesan = "Error dari Satu Sehat : ";
+        }
+
+        return {
+            status: error.response.status,
+            message: pesan + error.response.data.issue[0].details.text,
+            data: error.response.data
+        }
+    }
+}
+
 module.exports = {
-    buat
+    buat,
+    cari_by_id_pasien,
+    perbarui
 };
